e65beb2fcc5fbc4e1fda76e668b385f215cb3d35
Peter Osztodi <27630335+petero2018@users.noreply.github.com>
2026-06-09 12:12:07 +0100

    setup elementary

--- legalrank/macros/generate_schema_name.sql
+++ legalrank/macros/generate_schema_name.sql
@@ -1,6 +1,10 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- if custom_schema_name is not none -%}
         {{ custom_schema_name }}
+    {%- elif node.resource_type == 'seed' -%}
+        {{ node.config.schema | upper }}
+    {%- elif node.package_name == 'elementary' -%}
+        ELEMENTARY
     {%- else -%}
         {{ node.fqn[1] }}
     {%- endif -%}
